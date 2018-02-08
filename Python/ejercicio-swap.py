438b8ac8fe11f1af85c616f8a7b7b4be571518c4
Ferranm <36172362+Ferranm@users.noreply.github.com>
2018-02-08 21:58:54 +0100

    Create ejercicio-swap.py

---
+++ Python/ejercicio-swap.py
@@ -0,0 +1,14 @@
+# coding: utf8
+# Ferran March Azañero
+# 08/02/2018
+
+mano_der="Movil"
+mano_izq="Bocadillo"
+
+mano_temp=mano_der
+mano_der=mano_izq
+mano_izq=mano_temp
+
+print mano_izq
+print mano_der
+print mano_temp
