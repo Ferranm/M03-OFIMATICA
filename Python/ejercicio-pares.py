59cae3d9d6c991b2b35562a36d877f1cff4175ab
Ferranm <36172362+Ferranm@users.noreply.github.com>
2018-02-08 12:03:57 +0100

    Create ejercicio-pares.py

---
+++ Python/ejercicio-pares.py
@@ -0,0 +1,10 @@
+# coding: utf8
+# Ferran March Azañero
+# 07/02/2018
+
+Numero=input ("Introduce el numero deseado:")
+if Numero % 2 == 0:
+    print "Qué bonito número par" 
+
+else: 
+    print "Qué número mas vulgar"
