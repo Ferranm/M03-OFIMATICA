3b341debfa978a77cb3f8319165b8acd3ecc4951
Ferranm <36172362+Ferranm@users.noreply.github.com>
2018-02-08 12:04:25 +0100

    Create ejercicio-condicion-chunga.py

---
+++ Python/ejercicio-condicion-chunga.py
@@ -0,0 +1,7 @@
+# coding: utf8
+# Ferran March Azañero
+# 07/02/2018
+
+numero=input ("Introduce el numero:")
+if (numero % 2 == 0) or (numero>= -10 and numero <= 40) or (numero<0):
+    print "Cumples con la condicion chunga"
