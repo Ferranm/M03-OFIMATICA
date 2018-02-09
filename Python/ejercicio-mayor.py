74db3fc4f9e43e53607f72962e7f835c81f01d14
Ferranm <36172362+Ferranm@users.noreply.github.com>
2018-02-09 12:55:37 +0100

    Create ejercicio-mayor.py

---
+++ Python/ejercicio-mayor.py
@@ -0,0 +1,14 @@
+# coding: utf8
+# Ferran March Azañero
+# 09/02/2018
+
+numero1=input ("Introduce el primer numero:")
+numero2=input ("Introduce el segundo numero:")
+
+if(numero1>numero2):
+    print "El numero1 es mas grande que el numero2"
+else:
+    if(numero2>numero1):
+        print "El numero2 es mas grande que el numero1"
+    else:
+        print "Los numeros son iguales"
