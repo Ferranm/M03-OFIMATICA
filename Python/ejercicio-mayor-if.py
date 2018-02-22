8a60d7ce719cb5d3cfca15a7b8be53e04e98cd94
Ferranm <36172362+Ferranm@users.noreply.github.com>
2018-02-22 08:45:18 +0100

    Create ejercicio-mayor-if.py

---
+++ Python/ejercicio-mayor-if.py
@@ -0,0 +1,14 @@
+# coding: utf8
+# Ferran March Azañero
+# 21/02/2018
+
+Valor1=input ("Introduce el valor 1:")
+Valor2=input ("Introduce el valor 2:")
+
+if (Valor1>Valor2):
+    print "Valor1:", Valor1, "gana"
+else:
+    if (Valor1<Valor2):
+        print "Valor2:", Valor2, "gana"
+    else:
+        print "Son iguales"
